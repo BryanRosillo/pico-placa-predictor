21c475e3c66d1bc837a7098e40243e8ad87f8dd2
BryanRosillo <108194629+BryanRosillo@users.noreply.github.com>
2025-02-18 17:27:10 -0500

    feat: license plate number management for prediction

---
+++ src/main/java/com/stackbuilders/picoplaca/models/LicensePlateNumber.java
@@ -0,0 +1,29 @@
+package com.stackbuilders.picoplaca.models;
+
+public class LicensePlateNumber {
+
+    private String letters;
+    private String numbers;
+
+    public LicensePlateNumber(String licencePlateNumber) {
+        if (licencePlateNumber==null||licencePlateNumber.isEmpty()||!validateLicencePlateNumber(licencePlateNumber)) {
+            throw new IllegalArgumentException("Invalid licence plate number, please enter a valid value.");
+        }
+        String [] parts = licencePlateNumber.split("-");
+        this.letters = parts[0];
+        this.numbers = parts[1];
+    }
+
+    private boolean validateLicencePlateNumber(String licencePlateNumber) {
+        return licencePlateNumber.matches("^[A-Z]{3}-\\d{3,4}$");
+    }
+
+    public String getLetters() {
+        return letters;
+    }
+
+    public String getNumbers() {
+        return numbers;
+    }
+
+}
---
+++ src/test/java/LicensePlateTests/LicencePlateNumberUnitTests.java
@@ -0,0 +1,30 @@
+package LicensePlateTests;
+
+import com.stackbuilders.picoplaca.models.LicensePlateNumber;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LicencePlateNumberUnitTests {
+
+    @ParameterizedTest
+    @DisplayName("Test 01 - correct license plate number creation")
+    @ValueSource(strings = {"ABC-1234", "XYZ-5678", "QWE-987", "RTY-123"})
+    void testCreationLicensePlateNumber(String stringLicensePlateNumber){
+        LicensePlateNumber licensePlateNumber = new LicensePlateNumber(stringLicensePlateNumber);
+        assertNotNull(licensePlateNumber.getLetters());
+        assertNotNull(licensePlateNumber.getNumbers());
+    }
+
+    @ParameterizedTest
+    @DisplayName("Test 02 - incorrect license plate number creation")
+    @ValueSource(strings = {"ABCD-1234", "XYZ-56781", "QaWE-987", "RTY-1x23"})
+    void testIncorrectCreationLicensePlateNumber(String stringLicensePlateNumber){
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new LicensePlateNumber(stringLicensePlateNumber));
+        assertEquals("Invalid licence plate number, please enter a valid value.", exception.getMessage());
+    }
+
+}
